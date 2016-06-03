913d4d2817c992b122226bcb4f9fa709bd02555e
ALEJANDRO VELAZQUEZ <alejandrovelazquez@alejandros-mac.local>
2016-06-03 15:35:46 -0700

    Practica 03

---
+++ Lab03/StartScreen.h
@@ -0,0 +1,106 @@
+//
+//  ViewController.h
+//  Lab03
+//
+//  Created by ALEJANDRO VELAZQUEZ on 6/2/16.
+//  Copyright © 2016 AlRu Projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartScreen : UIViewController
+@property (strong, nonatomic) IBOutlet UIScrollView * scroller;
+
+
+//selector for caracter 1
+@property (strong, nonatomic) IBOutlet UIImageView *img_selected1;
+@property (strong, nonatomic) IBOutlet UIButton *button1;
+@property (strong, nonatomic) IBOutlet UIButton *button2;
+
+//button execution for caracter 1
+- (IBAction)button1:(id)sender; //Live
+- (IBAction)button2:(id)sender; //Died
+
+//selector for caracter 2
+//button execution for caracter 2
+@property (strong, nonatomic) IBOutlet UIImageView *img_selected2;
+@property (strong, nonatomic) IBOutlet UIButton *button3;
+@property (strong, nonatomic) IBOutlet UIButton *button4;
+
+- (IBAction)button3:(id)sender; //Live
+- (IBAction)button4:(id)sender; //Died
+
+//selector for caracter 3
+//button execution for caracter 3
+@property (strong, nonatomic) IBOutlet UIImageView *img_selected3;
+@property (strong, nonatomic) IBOutlet UIButton *button5;
+@property (strong, nonatomic) IBOutlet UIButton *button6;
+
+- (IBAction)button5:(id)sender; //Live
+- (IBAction)button6:(id)sender; //Died
+
+//selector for caracter 4
+//button execution for caracter 4
+@property (strong, nonatomic) IBOutlet UIImageView *img_selected4;
+@property (strong, nonatomic) IBOutlet UIButton *button7;
+@property (strong, nonatomic) IBOutlet UIButton *button8;
+
+- (IBAction)button7:(id)sender; //Live
+- (IBAction)button8:(id)sender; //Died
+
+//selector for caracter 5
+//button execution for caracter 5
+@property (strong, nonatomic) IBOutlet UIImageView *img_selected5;
+@property (strong, nonatomic) IBOutlet UIButton *button9;
+@property (strong, nonatomic) IBOutlet UIButton *button10;
+
+- (IBAction)button9:(id)sender;     //Live
+- (IBAction)button10:(id)sender;    //Died
+
+//selector for caracter 6
+//button execution for caracter 6
+@property (strong, nonatomic) IBOutlet UIImageView *img_selected6;
+@property (strong, nonatomic) IBOutlet UIButton *button11;
+@property (strong, nonatomic) IBOutlet UIButton *button12;
+
+- (IBAction)button11:(id)sender;    //Live
+- (IBAction)button12:(id)sender;    //Died
+
+//selector for caracter 7
+//button execution for caracter 7
+@property (strong, nonatomic) IBOutlet UIImageView *img_selected7;
+@property (strong, nonatomic) IBOutlet UIButton *button13;
+@property (strong, nonatomic) IBOutlet UIButton *button14;
+
+- (IBAction)button13:(id)sender;    //Live
+- (IBAction)button14:(id)sender;    //Died
+
+//selector for caracter 8
+//button execution for caracter 8
+@property (strong, nonatomic) IBOutlet UIImageView *img_selected8;
+@property (strong, nonatomic) IBOutlet UIButton *button15;
+@property (strong, nonatomic) IBOutlet UIButton *button16;
+
+- (IBAction)button15:(id)sender;    //Live
+- (IBAction)button16:(id)sender;    //Died
+
+//selector for caracter 9
+//button execution for caracter 9
+@property (strong, nonatomic) IBOutlet UIImageView *img_selected9;
+@property (strong, nonatomic) IBOutlet UIButton *button17;
+@property (strong, nonatomic) IBOutlet UIButton *button18;
+
+- (IBAction)button17:(id)sender;    //Live
+- (IBAction)button18:(id)sender;    //Died
+
+//selector for caracter 10
+//button execution for caracter 10
+@property (strong, nonatomic) IBOutlet UIImageView *img_selected10;
+@property (strong, nonatomic) IBOutlet UIButton *button19;
+@property (strong, nonatomic) IBOutlet UIButton *button20;
+
+- (IBAction)button19:(id)sender;    //Live
+- (IBAction)button20:(id)sender;    //Died
+
+@end
+
---
+++ Lab03/StartScreen.m
@@ -0,0 +1,186 @@
+//
+//  ViewController.m
+//  Lab03
+//
+//  Created by ALEJANDRO VELAZQUEZ on 6/2/16.
+//  Copyright © 2016 AlRu Project	s. All rights reserved.
+//
+
+#import "StartScreen.h"
+
+@interface StartScreen ()
+
+@end
+
+@implementation StartScreen
+
+static int cont = 0;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    [self.scroller setScrollEnabled:YES];
+    [self.scroller setContentSize:CGSizeMake(320, 1480)];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+// Task Caracter 1
+- (IBAction)button1:(id)sender {
+    self.img_selected1.image = [ UIImage imageNamed: @"correct.png"];
+    cont++;
+    //disable buttons
+    self.button1.enabled = FALSE;
+    self.button2.enabled = FALSE;
+    
+}
+
+- (IBAction)button2:(id)sender {
+    self.img_selected1.image = [ UIImage imageNamed: @"incorrect.png"];
+    //disable buttons
+    self.button1.enabled = FALSE;
+    self.button2.enabled = FALSE;
+}
+// Task Caracter 2
+- (IBAction)button3:(id)sender {
+    self.img_selected2.image = [ UIImage imageNamed: @"incorrect.png"];
+    //disable buttons
+    self.button3.enabled = FALSE;
+    self.button4.enabled = FALSE;
+}
+
+- (IBAction)button4:(id)sender {
+    self.img_selected2.image = [ UIImage imageNamed: @"correct.png"];
+    cont++;
+    //disable buttons
+    self.button3.enabled = FALSE;
+    self.button4.enabled = FALSE;
+    
+}
+// Task Caracter 3
+- (IBAction)button5:(id)sender {
+    self.img_selected3.image = [ UIImage imageNamed: @"correct.png"];
+    cont++;
+    //disable buttons
+    self.button5.enabled = FALSE;
+    self.button6.enabled = FALSE;
+}
+
+- (IBAction)button6:(id)sender {
+    self.img_selected3.image = [ UIImage imageNamed: @"correct.png"];
+    cont++;
+    //disable buttons
+    self.button5.enabled = FALSE;
+    self.button6.enabled = FALSE;
+}
+// Task Caracter 4
+- (IBAction)button7:(id)sender {
+    self.img_selected4.image = [ UIImage imageNamed: @"correct.png"];
+    cont++;
+    //disable buttons
+    self.button7.enabled = FALSE;
+    self.button8.enabled = FALSE;
+}
+
+- (IBAction)button8:(id)sender {
+    self.img_selected4.image = [ UIImage imageNamed: @"incorrect.png"];
+    //disable buttons
+    self.button7.enabled = FALSE;
+    self.button8.enabled = FALSE;
+}
+// Task Caracter 5
+- (IBAction)button9:(id)sender {
+    self.img_selected5.image = [ UIImage imageNamed: @"correct.png"];
+    cont++;
+    //disable buttons
+    self.button9.enabled = FALSE;
+    self.button10.enabled = FALSE;
+}
+
+- (IBAction)button10:(id)sender {
+    self.img_selected5.image = [ UIImage imageNamed: @"incorrect.png"];
+    //disable buttons
+    self.button9.enabled = FALSE;
+    self.button10.enabled = FALSE;
+}
+// Task Caracter 6
+- (IBAction)button11:(id)sender {
+    self.img_selected6.image = [ UIImage imageNamed: @"incorrect.png"];
+    //disable buttons
+    self.button11.enabled = FALSE;
+    self.button12.enabled = FALSE;
+}
+
+- (IBAction)button12:(id)sender {
+    self.img_selected6.image = [ UIImage imageNamed: @"correct.png"];
+    cont++;
+    //disable buttons
+    self.button11.enabled = FALSE;
+    self.button12.enabled = FALSE;
+}
+// Task Caracter 7
+- (IBAction)button13:(id)sender {
+    self.img_selected7.image = [ UIImage imageNamed: @"incorrect.png"];
+    //disable buttons
+    self.button13.enabled = FALSE;
+    self.button14.enabled = FALSE;
+}
+
+- (IBAction)button14:(id)sender {
+    self.img_selected7.image = [ UIImage imageNamed: @"correct.png"];
+    cont++;
+    //disable buttons
+    self.button13.enabled = FALSE;
+    self.button14.enabled = FALSE;
+}
+// Task Caracter 8
+- (IBAction)button15:(id)sender {
+    self.img_selected8.image = [ UIImage imageNamed: @"correct.png"];
+    cont++;
+    //disable buttons
+    self.button15.enabled = FALSE;
+    self.button16.enabled = FALSE;
+}
+
+- (IBAction)button16:(id)sender {
+    self.img_selected8.image = [ UIImage imageNamed: @"incorrect.png"];
+    //disable buttons
+    self.button15.enabled = FALSE;
+    self.button16.enabled = FALSE;
+}
+// Task Caracter 9
+- (IBAction)button17:(id)sender {
+    self.img_selected9.image = [ UIImage imageNamed: @"correct.png"];
+    cont++;
+    //disable buttons
+    self.button17.enabled = FALSE;
+    self.button18.enabled = FALSE;
+}
+
+- (IBAction)button18:(id)sender {
+    self.img_selected9.image = [ UIImage imageNamed: @"incorrect.png"];
+    //disable buttons
+    self.button17.enabled = FALSE;
+    self.button18.enabled = FALSE;
+}
+// Task Caracter 10
+- (IBAction)button19:(id)sender {
+    self.img_selected10.image = [ UIImage imageNamed: @"correct.png"];
+    cont++;
+    //disable buttons
+    self.button19.enabled = FALSE;
+    self.button20.enabled = FALSE;
+}
+
+- (IBAction)button20:(id)sender {
+    self.img_selected10.image = [ UIImage imageNamed: @"incorrect.png"];
+    //disable buttons
+    self.button19.enabled = FALSE;
+    self.button20.enabled = FALSE;
+}
+
+
+@end
